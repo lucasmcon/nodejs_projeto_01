9cb04d3cbd27728889b465e2c56fe59ed6ee879c
Lucas Menezes <39992002+lucasmcon@users.noreply.github.com>
2020-02-18 10:14:32 -0300

    Correção de e querys
    Corerção de querys e body parse.

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ app.get("/perguntar",(req, res) => {
 
 app.post("/salvarpergunta",(req, res) => {
 
-    var titulo = req.body.titulo;
-    var descricao = req.body.descricao;
+    var per_st_tit = req.body.per_st_tit;
+    var per_st_desc = req.body.per_st_desc;
 
     Pergunta.create({
-        titulo: titulo,
-        descricao: descricao
+        per_st_tit: per_st_tit,
+        per_st_desc: per_st_desc
     }).then(() => {
         res.redirect("/");
     });
@@ -75,14 +75,14 @@ app.get("/pergunta/:id",(req ,res) => {
 })
 
 app.post("/responder",(req, res) => {
-    var corpo = req.body.corpo;
-    var perguntaId = req.body.pergunta;
+    var res_st_desc = req.body.res_st_desc;
+    var per_in_id = req.body.per_in_id;
     Resposta.create({
-        corpo: corpo,
-        perguntaId: perguntaId
+        res_st_desc: res_st_desc,
+        per_in_id: per_in_id
     }).then(() => {
-        res.redirect("/pergunta/"+perguntaId);
+        res.redirect("/pergunta/"+per_in_id);
     });
 });
 
-app.listen(8080,()=>{console.log("App rodando!");})
+app.listen(8090,()=>{console.log("App rodando!");})
