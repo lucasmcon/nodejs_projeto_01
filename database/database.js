0d3f9abbf837421a848a5e1f636ab2085fb2f8a9
Lucas Menezes <lucasmcon@unifaat.edu.br>
2020-02-18 00:23:58 -0300

    Atualização

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiaperguntas','root','Qwaszx123*',{
+const connection = new Sequelize('guiaperguntas','root','',{
     host: 'localhost',
     dialect: 'mysql'
 });
