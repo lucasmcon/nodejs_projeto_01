00147f12521382ab71d3bd3d5fa227fb26220132
Lucas Menezes <lucasmcon@gmail.com>
2021-11-05 10:58:25 -0300

    body parser remove
    body parser deprecated. Express faz essa função agora.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
 const Resposta = require("./database/Resposta");
+
 //Database
 connection
     .authenticate()
@@ -18,8 +18,8 @@ connection
 app.set('view engine','ejs');
 app.use(express.static('public'));
 // Body parser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Rotas
 app.get("/",(req, res) => {
